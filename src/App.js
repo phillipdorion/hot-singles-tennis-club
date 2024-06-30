f4e6fa271aff60b4ff179b7d385e69b2879e37bb
Phil Dorion <phildorion@Phils-Mac-mini.local>
2024-06-30 13:52:24 -0600

    new versions

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { ChakraProvider, Box } from '@chakra-ui/react';
+import { AnimatePresence, motion } from 'framer-motion'
 import Navbar from './components/Navbar';
 import Home from './components/Home';
 import MatchLogging from './components/MatchLogging';
